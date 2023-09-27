bb69f01345559e7420f6ee74f0b99fe185734138
jonghyeon <kiri6077@naver.com>
2023-09-27 15:36:11 +0900

    로그인 로직 구현 실습 완료

--- src/main/java/com/example/todo/config/WebSecurityConfig.java
+++ src/main/java/com/example/todo/config/WebSecurityConfig.java
@@ -1,7 +1,15 @@
 package com.example.todo.config;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 // import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -10,12 +18,22 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.filter.CorsFilter;
 
 import com.example.todo.security.JwtAuthenticationFilter;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
+@Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig {
+
+    private final ObjectMapper objectMapper;
+
+    @Autowired
+    public WebSecurityConfig(ObjectMapper objectMapper) {
+        this.objectMapper = objectMapper;
+    }
 
     @Autowired
     private JwtAuthenticationFilter jwtAuthenticationFilter;
@@ -37,6 +55,18 @@ public class WebSecurityConfig {
                 .permitAll()
                 .anyRequest()
                 .authenticated();
+
+        http.exceptionHandling()
+                .authenticationEntryPoint((request, response, e) -> {
+                    Map<String, Object> data = new HashMap<String, Object>();
+                    data.put("status", HttpServletResponse.SC_FORBIDDEN);
+                    data.put("message", e.getMessage());
+
+                    response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+                    response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+
+                    objectMapper.writeValue(response.getOutputStream(), data);
+                });
 
         http.addFilterAfter(jwtAuthenticationFilter, CorsFilter.class);
 
