306ef89c8418d98cd0edfc8dc21dd21003dc4493
jonghyeon <kiri6077@naver.com>
2023-09-27 14:59:52 +0900

    WebSecurityConfigurerAdapter 관련 문제 해결

--- src/main/java/com/example/todo/config/WebSecurityConfig.java
+++ src/main/java/com/example/todo/config/WebSecurityConfig.java
@@ -1,10 +1,12 @@
 package com.example.todo.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+// import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.filter.CorsFilter;
 
 import com.example.todo.security.JwtAuthenticationFilter;
@@ -13,13 +15,13 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @EnableWebSecurity
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+public class WebSecurityConfig {
 
     @Autowired
     private JwtAuthenticationFilter jwtAuthenticationFilter;
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    @Bean
+    protected SecurityFilterChain configure(HttpSecurity http) throws Exception {
         http.cors()
                 .and()
                 .csrf()
@@ -30,10 +32,14 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .authorizeRequests()
-                .antMatchers("/", "/auth/**").permitAll()
+                .antMatchers("/", "/auth/**",
+                        "/h2-console/**")
+                .permitAll()
                 .anyRequest()
                 .authenticated();
 
         http.addFilterAfter(jwtAuthenticationFilter, CorsFilter.class);
+
+        return http.build();
     }
 }
